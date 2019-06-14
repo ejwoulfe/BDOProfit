5faecbeda04197d330aa83a405931d1552907d6c
Ed Woulfe <47793125+ejwoulfe@users.noreply.github.com>
2019-06-14 10:15:49 -0500

    Update connectToDatabase.php

--- Includes/connectToDatabase.php
+++ Includes/connectToDatabase.php
@@ -1,13 +1,3 @@
 <?php
-$username = 'root';
-$password = 'root';
-$db = 'bdowolf_database';
-$host = 'localhost';
-$port = 8890;
-
-$conn = new mysqli("localhost", "root", "root", "bdowolf_database", 8890);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-
-}
+// Secret Information :)
 ?>
