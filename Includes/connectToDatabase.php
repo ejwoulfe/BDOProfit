84a90a289d9e0520d4708538c83a6ae26f5e3859
Ed Woulfe <47793125+ejwoulfe@users.noreply.github.com>
2019-08-21 10:40:24 -0500

    Update connectToDatabase.php

--- Includes/connectToDatabase.php
+++ Includes/connectToDatabase.php
@@ -1,10 +1,1 @@
-<?php
-$username = 'bdoprofit_user';
-$password = 'middleEarth@03368!';
-$db = 'bdowolf_database';
-$host = 'edwoulfe90950.ipagemysql.com';
-$conn = new mysqli("edwoulfe90950.ipagemysql.com", "bdoprofit_user", "middleEarth@03368!", "bdowolf_database");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-?>
+Secret info!
